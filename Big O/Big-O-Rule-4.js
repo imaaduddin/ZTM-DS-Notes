226c404d30e742453e5100da6c70a55442533b0c
Imaad Uddin <imaaduddin27@gmail.com>
2022-06-13 19:25:07 -0500

    big o rule 4

--- Big O/Big-O-Rule-4.js
+++ Big O/Big-O-Rule-4.js
@@ -1,1 +1,17 @@
 // Rule 4: Drop Non Dominants 
+
+function printAllNumbersThenAllPairSums(numbers) {
+    console.log('These are the numbers');
+    numbers.forEach((number) => {
+        console.log(number);
+    });
+
+    console.log('and these are their sums:');
+    numbers.forEach((firstNumber) => {
+        numbers.forEach(secondNumber => {
+            console.log(firstNumber + secondNumber);
+        });
+    });
+}
+
+printAllNumbersThenAllPairSums([1,2,3,4,5]);
