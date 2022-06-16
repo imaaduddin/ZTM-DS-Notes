0ef6faeafaa782cba8e036751d2459be1c2d19fb
Imaad Uddin <imaaduddin27@gmail.com>
2022-06-16 15:18:03 -0500

    space complexity

---
+++ Big O/Space-Complexity.js
@@ -0,0 +1,17 @@
+function booo(n) {
+    for (let i = 0; i < n.length; i++) {
+        console.log('booooo!');
+    }
+}
+
+booo([1,2,3,4,5,6,7,8,9]); // O(1)
+
+function arrayOfHiNTimes(n) {
+    let hiArray = [];
+    for (let i = 0; i < n; i++) {
+        hiArray[i] = 'hi';
+    }
+    return hiArray;
+}
+
+console.log(arrayOfHiNTimes(6));
