d66225eed1be4a03b6d859874385ead88edd037b
Imaad Uddin <imaaduddin27@gmail.com>
2022-06-20 18:54:57 -0500

    different ways to loop over an array

---
+++ Looping/loops.js
@@ -0,0 +1,37 @@
+// Different ways to loop over an array!
+
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'squirt', 'darla', 'hank'];
+const large = new Array(100000).fill('nemo');
+
+const findNemo = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'nemo') {
+            console.log('Found Nemo!');
+        }
+    }
+}
+
+findNemo(everyone);
+
+// foreach
+const findNemo2 = (array) => {
+    array.forEach(fish => {
+        if (fish === 'nemo') {
+            console.log('Found nemo!');
+        }
+    });
+}
+
+findNemo2(everyone);
+
+// For Of
+const findNemo3 = (array) => {
+    for (let fish of array) {
+        if (fish === 'nemo') {
+            console.log('Found Nemo!');
+        }
+    }
+}
+
+findNemo3(everyone);
