7f18f5ca661859af8ef67ad402ec97fe2d17ffa1
Imaad Uddin <imaaduddin27@gmail.com>
2022-06-30 15:30:00 -0500

    more efficient solution

--- Sample Interview Question/question1.js
+++ Sample Interview Question/question1.js
@@ -9,17 +9,23 @@ const array2 = ['z', 'y', 'x'];
 // should return true 
 
 // My solution
-function checkArray(array1, array2) {
-    for (let i = 0; i < array1.length; i++) {
-        for (let j = 0; j < array2.length; j++) {
-            if (array1[i] === array2[j]) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
+// function checkArray(array1, array2) {
+//     for (let i = 0; i < array1.length; i++) {
+//         for (let j = 0; j < array2.length; j++) {
+//             if (array1[i] === array2[j]) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
 
 // O(a*b)
+
+// Better solution (more efficient)
+function checkArray(array1, array2) {
+    // loop through the first array and create a object where properties === item in the array 
+    // loop through second array and check if item in sec ond array exists on created object
+}
 
 checkArray(array1, array2);
