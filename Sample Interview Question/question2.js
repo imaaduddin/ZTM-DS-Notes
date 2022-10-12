180a2c2e29155a385bfd900fa28f82b6ef8880b8
Imaad Uddin <imaaduddin27@gmail.com>
2022-10-12 17:23:29 -0500

    practice question

---
+++ Sample Interview Question/question2.js
@@ -0,0 +1,22 @@
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'a'];
+
+function containsCommonItem2(arr1, arr2) {
+    // loop through first array and create object where properties === items in the array 
+    let map = {};
+    for (let i = 0; i < arr1.length; i++) {
+        if (!map[arr1[i]]) {
+            const item = arr1[i];
+            map[item] = true;
+        }
+    }
+    // loop through the second array and check if item in the second array exists on created object
+    for (let j = o; j < arr2.length; j++) {
+        if (map[arr2[j]]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+containsCommonItem2(array1, array2);
